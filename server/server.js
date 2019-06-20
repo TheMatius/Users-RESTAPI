a29c241dab69959906f09fa4e795ce486c82d989
Matius Monsalve <thematius.monsalve@gmail.com>
2019-06-19 19:09:43 -0500

    test4 mongodb

--- server/server.js
+++ server/server.js
@@ -12,7 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/user.route'));
 
-mongoose.connect(process.env.urlDB, { useNewUrlParser: true })
+let options = {
+    useNewUrlParser: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 100, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+}
+
+mongoose.connect(process.env.urlDB, options)
     .then(() => console.log('Database online!'))
     .catch(err => {
         if (err) throw err;
