193b895066c20b1259f11b4a84997490aa2a42bb
Matius Monsalve <thematius.monsalve@gmail.com>
2019-06-19 19:37:34 -0500

    Test1 mongodb

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ let options = {
     reconnectTries: 100, // Never stop trying to reconnect
     reconnectInterval: 500, // Reconnect every 500ms
     poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
     bufferMaxEntries: 0
 };
 
