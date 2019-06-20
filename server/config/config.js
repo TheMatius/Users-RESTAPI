f0fcf8b38aec815e213fbf71edc13d68780ba80e
Matius Monsalve <thematius.monsalve@gmail.com>
2019-06-19 19:11:53 -0500

    test5 mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -12,12 +12,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //         Database
 // ========================
 let urlDB;
-let password = encodeURI('12345');
+// let password = encodeURI('12345');
 
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb://localhost:27017/coffee';
 // } else {
-urlDB = `mongodb+srv://admin:${ password }@coffee-jqhuw.mongodb.net/test?retryWrites=true&w=majority`;
+urlDB = `mongodb+srv://admin:12345@coffee-jqhuw.mongodb.net/test?retryWrites=true&w=majority`;
 // }
 
 process.env.urlDB = urlDB;
